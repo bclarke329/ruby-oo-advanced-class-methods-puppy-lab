67d9f371dc1725a936823a70e8c493eece69ff36
bclarke329 <bmendoza329@hotmail.com>
2020-10-14 20:02:56 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
 # Add your code here
+require 'pry'
+class Dog
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @i = save
+    end
+
+    def self.all
+        @@all
+    end 
+
+    def self.clear_all
+        @@all.clear
+    end 
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end 
+     end 
+
+    def save 
+        @@all << self
+    end 
+
+    def name
+        @name
+    end 
+
+end 
